7b1b91a9195852ec8340463d60aade50691f7803
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-05-28 20:03:08 +0000

    add colum

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,11 +47,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
          $this->validate($request, [
-             //'status' => 'required|max:10',
+             'status' => 'required|max:10',
             'content' => 'required|max:191',
         ]);
         
         $task = new Task;
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
