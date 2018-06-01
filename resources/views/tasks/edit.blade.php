522cb46e3e48757146923725d5de10e43bbb46af
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-06-01 04:11:50 +0000

    complete

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -7,7 +7,7 @@
  
  <!--error用のコード deleted-->
      <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-sm-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
