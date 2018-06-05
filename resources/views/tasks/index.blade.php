543068903eb257dd426e9c0ebacf1a9e9d258363
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-06-05 03:45:21 +0000

    tasklist 1

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -38,6 +38,6 @@
         </table>
     @endif
     
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '新規タスクの追加', null, ['class' => 'btn btn-primary']) !!}
    
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,25 @@
 });
 */
 
-Route::get('/', 'TasksController@index');
 
-Route::resource('tasks', 'TasksController');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/', 'TasksController@index');
+
+// user registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// Login authentication
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+//authentication
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('tasks', 'TasksController');
+});
